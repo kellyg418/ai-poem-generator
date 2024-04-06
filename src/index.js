f9d6b3dca2f06a0ac54aeed99e8d721bda160e2e
kellyg418 <kellygranados66@gmail.com>
2024-04-05 17:31:06 -0700

    Edited API context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "tf3af8fe40boeff366fbea30e0d4a794";
-    let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line. Make sure to follow the user instructions. Do not include a title for the poems."
+    let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line. Do not display each line together at any point. Make sure to follow the user instructions. Do not include a title for the poems."
     let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
     let apiURL= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
