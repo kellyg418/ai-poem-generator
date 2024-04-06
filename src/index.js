15ef1282e3d80b99a7ec6c15208f04517c0c26b4
kellyg418 <kellygranados66@gmail.com>
2024-04-05 17:07:03 -0700

    Added Axios to the index, added more basic CSS, and integrated AI API key to produce poem generation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
+    console.log("poem generated");
     new Typewriter("#poem", {
-        strings: "poem will be here",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
-    });
+    }); 
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "tf3af8fe40boeff366fbea30e0d4a794";
+    let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line. Make sure to follow the user instructions. Do not include a title for the poems."
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    let apiURL= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log("Generating poem");
+console.log(`Prompt: ${prompt}`);
+console.log(`Context: ${context}`);
+
+    axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
